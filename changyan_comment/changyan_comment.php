fa017a9afcd65307d3323562e8004634c5394fa3
Techmoe <developershi@sina.cn>
2015-06-01 13:19:53 +0000

    First commit

---
+++ changyan_comment/changyan_comment.php
@@ -0,0 +1,35 @@
+<?php
+
+$wgExtensionCredits['specialpage'][] = array(
+		'path'              => __FILE__,
+		'name'              => 'changyan4mw',
+		'version'           => '0.2',
+		'author'            => 'techmoe',
+		'description'       => 'Mediawiki 畅言评论插件',
+		'descriptionmsg'    => '',
+		'url'               => 'http://cnblogs.com/techmoe'
+);
+
+$wgHooks['SkinAfterContent'][] = 'Changyan::onSkinAfterContent';
+//$wgHooks['SkinAfterBottomScripts'][] = 'Changyan::onSkinAfterBottomScripts';
+
+class Changyan {
+	public static function onSkinAfterBottomScripts($skin, &$text){
+		echo "testing..";
+		return true;
+	}
+	public static function onSkinAfterContent(&$data, $skin = null){
+		$data .='<!--高速版-->
+<div id="SOHUCS"></div>
+<script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
+<script type="text/javascript">
+    window.changyan.api.config({
+        appid: "cyrNJaVbr",
+        conf: "prod_c780bd492562f1b64ac67a1c591bb236"
+    });
+</script>        
+';
+		return true;
+	}
+}
+?>
